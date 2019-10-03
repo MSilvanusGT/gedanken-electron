9b844d5349840752a98d3b97cb36288c097408f6
Mostafa Elgayar <meeger99@gmail.com>
2019-10-03 02:41:31 +0200

    ui

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC1vnxdewfjJEGbenKqD7e_fe3Dx4TAgUo",
+    authDomain: "gedanken-f0f1e.firebaseapp.com",
+    databaseURL: "https://gedanken-f0f1e.firebaseio.com",
+    projectId: "gedanken-f0f1e",
+    storageBucket: "gedanken-f0f1e.appspot.com",
+    messagingSenderId: "2819382580",
+    appId: "1:2819382580:web:4377c1b0ac43b8576e1961"
+  };
+
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
---
+++ get.js
@@ -0,0 +1,6 @@
+    db.collection("keywordsToLinks").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        let div = document.getElementById('data');
+        let element = document.createElement(  )
+    });
+});
